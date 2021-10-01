5db22be53e1697bff7ce136f2decf9094cd1f907
Yuriy Ivanov <ymivanov@icloud.com>
2021-10-01 18:12:58 +0300

    <HomeAbout> component responsive styling

--- components/home/HomeAbout.tsx
+++ components/home/HomeAbout.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import { Grid } from 'semantic-ui-react';
 // styles //
-import homeAboutStyles from "../../styles/home/HomeAbout.module.css";
+import styles from "@/styles/home/HomeAbout.module.css";
 
 interface IHomeAboutProps {
 }
 
 export const HomeAbout: React.FunctionComponent<IHomeAboutProps> = (props): JSX.Element => {
   return (
-    <Grid.Row className={ homeAboutStyles.wrapperRow }>
-      <div className={ homeAboutStyles.aboutDiv }>
-        <span>We are </span>
+    <Grid.Row className={ styles.wrapperRow }>
+      <div className={ styles.aboutDiv }>
+        <div className={ styles.aboutDivContent }>
+          <h4>Technology doesn&apos;t have to be confusing</h4>
+        </div>
       </div>
-      <div className={ homeAboutStyles.aboutDescriptionDiv }>
-        <div className={ homeAboutStyles.descriptionWrapper }>
-          <div className={ homeAboutStyles.descriptionContent }>
+      <div className={ styles.aboutDescriptionDiv }>
+        <div className={ styles.descriptionWrapper }>
+          <div className={ styles.descriptionContent }>
             A small constantly changing team of individuals looking to learn, teach, create and develop. 
             We work with various current technologies depending on the scope and demand of the project.
             Also theres a cat but he&apos;s not too friendly.
