36510f2c4ab5e46f8a9918e766b6efdb65428d7f
Yuriy Ivanov <ymivanov@icloud.com>
2021-10-28 18:52:58 +0300

    Merge API tests for BlogPostController:ToggleLike method, like - unlike for logged in User, CONTRIBUTOR account

--- server/spec/tests/api/blog_posts/blogPostAPI_PATCH_Likes.spec.ts
+++ server/spec/tests/api/blog_posts/blogPostAPI_PATCH_Likes.spec.ts
@@ -248,7 +248,121 @@ describe("BlogPostsController:ToggleLike PATCH - TOGGLE Like API Tests", () => {
       });
     });
   });
+  // END User client READER account //
 
+  // TEST User Client CONTRIBUTOR account //
+  context("User client - Logged in - CONTRIBUTOR account", () => {
+    let firstUsersPostId: string;
+    let _editedBlogPost: BlogPostData;
+    before(() => {
+      firstUsersPostId = firstUsersBlogPost._id.toHexString();
+    });
+    // TEST LIKE BLOGPOST //
+    describe("PATCH /api/posts/toggle_like/:post_id - Logged in - CONTRIBUTOR - LIKE response", () => {
+      it("Should update an existing <BlogPost> model, ADD LIKE and send back a correct response", (done) => {
+        chai.request(server)
+          .patch(`/api/posts/toggle_like/${firstUsersPostId}`)
+          .set({ Authorization: contributorToken })
+          .end((err, response) => {
+            if (err) done(err);
+            const { status } = response;
+            const { responseMsg, editedBlogPost, error, errorMessages } = response.body as EditBlogPostRes;
+            expect(status).to.equal(200);
+            expect(responseMsg).to.be.a("string");
+            expect(editedBlogPost).to.be.an("object");
+            // 
+            expect(error).to.be.undefined;
+            expect(errorMessages).to.be.undefined;
+            //
+            _editedBlogPost = editedBlogPost;
+            done();
+          })
+      });
+      it("Should alter the queried <BlogPost> model and ONLY add a LIKE", async () => {
+        try {
+          // what stayed the same //
+          expect(_editedBlogPost._id).to.equal(firstUsersBlogPost._id.toHexString());
+          expect(_editedBlogPost.title).to.equal(firstUsersBlogPost.title);
+          expect(_editedBlogPost.content).to.equal(firstUsersBlogPost.content);
+          expect(_editedBlogPost.keywords).to.eql(firstUsersBlogPost.keywords);
+          expect(_editedBlogPost.author.authorId).to.equal(firstUsersBlogPost.author.authorId.toHexString());
+          expect(_editedBlogPost.author.name).to.equal(firstUsersBlogPost.author.name);
+          // changes //
+          expect(_editedBlogPost.likes.length).to.eql(firstUsersBlogPost.likes.length + 1);
+          expect(_editedBlogPost.numOflikes).to.equal(firstUsersBlogPost.numOflikes + 1);
+          expect(_editedBlogPost.likes.some((userId) => contributorUser._id.equals(userId))).to.equal(true);
+        } catch (error) {
+          throw error;
+        }
+      });
+      it("Should NOT alter the number of <BlogPost> models in the database", async () => {
+        try {
+          const updatedNumOfBlogPosts: number = await BlogPost.countDocuments();
+          const updatedFirstUsersBlogPost: IBlogPost = await BlogPost.findOne({ _id: firstUsersPostId });
+          //
+          expect(updatedNumOfBlogPosts).to.equal(numberOfBlogPosts);
+          //
+          firstUsersBlogPost = updatedFirstUsersBlogPost;
+        } catch (error) {
+          throw error;
+        }
+      });
+    });
+    // END TEST LIKE BLOGPOST //
+
+    // TEST Remove BlogPost LIKE //
+    describe("PATCH /api/posts/toggle_like/:post_id - Logged in  - UNLIKE response", () => {
+      it("Should update an existing <BlogPost> model, REMOVE LIKE and send back a correct response", (done) => {
+        chai.request(server)
+          .patch(`/api/posts/toggle_like/${firstUsersPostId}`)
+          .set({ Authorization: contributorToken })
+          .end((err, response) => {
+            if (err) done(err);
+            const { status } = response;
+            const { responseMsg, editedBlogPost, error, errorMessages } = response.body as EditBlogPostRes;
+            expect(status).to.equal(200);
+            expect(responseMsg).to.be.a("string");
+            expect(editedBlogPost).to.be.an("object");
+            // 
+            expect(error).to.be.undefined;
+            expect(errorMessages).to.be.undefined;
+            //
+            _editedBlogPost = editedBlogPost;
+            done();
+          })
+      });
+      it("Should alter the queried <BlogPost> model and ONLY remove a LIKE", async () => {
+        try {
+          // what stayed the same //
+          expect(_editedBlogPost._id).to.equal(firstUsersBlogPost._id.toHexString());
+          expect(_editedBlogPost.title).to.equal(firstUsersBlogPost.title);
+          expect(_editedBlogPost.content).to.equal(firstUsersBlogPost.content);
+          expect(_editedBlogPost.keywords).to.eql(firstUsersBlogPost.keywords);
+          expect(_editedBlogPost.author.authorId).to.equal(firstUsersBlogPost.author.authorId.toHexString());
+          expect(_editedBlogPost.author.name).to.equal(firstUsersBlogPost.author.name);
+          // changes //
+          expect(_editedBlogPost.likes.length).to.eql(firstUsersBlogPost.likes.length - 1);
+          expect(_editedBlogPost.numOflikes).to.equal(firstUsersBlogPost.numOflikes - 1);
+          expect(_editedBlogPost.likes.some((userId) =>contributorUser._id.equals(userId))).to.equal(false);
+        } catch (error) {
+          throw error;
+        }
+      });
+      it("Should NOT alter the number of <BlogPost> models in the database", async () => {
+        try {
+          const updatedNumOfBlogPosts: number = await BlogPost.countDocuments();
+          const updatedFirstUsersBlogPost: IBlogPost = await BlogPost.findOne({ _id: firstUsersPostId });
+          //
+          expect(updatedNumOfBlogPosts).to.equal(numberOfBlogPosts);
+          //
+          firstUsersBlogPost = updatedFirstUsersBlogPost;
+        } catch (error) {
+          throw error;
+        }
+      });
+    });
+  });
+  // END Test User Client CONTRIBUTOR account //
   after(async () => {
     try {
       await Admin.deleteMany({});
