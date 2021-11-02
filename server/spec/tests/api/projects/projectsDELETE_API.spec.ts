2e683328fa75b1f87c2b013771e1a43d496ba9d3
Yuriy Ivanov <ymivanov@icloud.com>
2021-11-02 03:03:35 +0200

    Merge setup for ProjectsController:Delete action API tests

---
+++ server/spec/tests/api/projects/projectsDELETE_API.spec.ts
@@ -0,0 +1,82 @@
+import chai, { expect } from "chai";
+import chaiHTTP from "chai-http";
+// server //
+import { ServerInstance } from "../../../../src/server";
+//
+import Admin from "../../../../src/models/Admin";
+import User from "../../../../src/models/User";
+import Project from "@/server/src/models/Project";
+// types //
+import type { Express} from "express";
+import type { IAdmin } from "@/server/src/models/Admin";
+import type { IUser } from "@/server/src/models/User";
+import type { IProject } from "@/server/src/models/Project";
+import type { EditProjectRes } from "@/redux/_types/projects/dataTypes";
+import type { ProjectData } from "@/server/src/_types/projects/projectTypes";
+// helpers //
+import { generateMockAdmins, generateMockProjects, generateMockUsers } from "../../../../src/_helpers/mockDataGeneration";
+import { generateMockProjectData } from "../../../hepers/testHelpers";
+import { loginUser } from "../../../hepers/testHelpers";
+
+chai.use(chaiHTTP);
+
+// At the moment only <owner> level admins should be able to DELETE any Project model //
+describe("ProjectsController PATCH API tests", function () {
+  // custom timeout //
+  this.timeout(5000);
+  let mockProjectData: ProjectData;
+  //
+  let adminUser: IAdmin;
+  let ownerUser: IAdmin;
+  //
+  let readerRegUser: IUser;
+  let contributorRegUser: IUser;
+  //
+  let project: IProject;
+  //
+  let adminJWTToken: string;
+  let ownerJWTToken: string;
+  let readerJWTToken: string;
+  let contributorJWTToken: string;
+  //
+  let server: Express;
+  let numberOfProjects: number;
+
+  // generate data, set server //
+  before(async () => {
+    try {
+      await generateMockAdmins(1, "admin");
+      await generateMockAdmins(1, "owner");
+      await generateMockUsers({ number: 1, confirmed: true, type: "READER" });
+      await generateMockUsers({ number: 1, confirmed: true, type: "CONTRIBUTOR" });
+      // admins //
+      adminUser = await Admin.findOne({ role: "admin" });
+      ownerUser = await Admin.findOne({ role: "owner" });
+      readerRegUser = await User.findOne({ userType: "READER" });
+      contributorRegUser = await User.findOne({ userType: "CONTRIBUTOR" });
+      //
+      await generateMockProjects(5, { published: true, creator: ownerUser._id.toHexString() });
+      await generateMockProjects(5, { published: false, creator: ownerUser._id.toHexString() });
+      //
+      project = await Project.findOne({ published: true });
+      //
+      numberOfProjects = await Project.countDocuments();
+      mockProjectData = generateMockProjectData();
+      server = ServerInstance.getExpressServer();
+      //
+    } catch (error) {
+      throw error;
+    }
+  });
+  // login all users  //
+  before(async () => {
+    try {
+      ({ userJWTToken: adminJWTToken } = await loginUser({ chai, server, email: adminUser.email }));
+      ({ userJWTToken: ownerJWTToken } = await loginUser({ chai, server, email: ownerUser.email }));
+      ({ userJWTToken: readerJWTToken } = await loginUser({ chai, server, email: readerRegUser.email }));
+      ({ userJWTToken: contributorJWTToken } = await loginUser({ chai, server, email: contributorRegUser.email }));
+    } catch (error) {
+      throw (error);
+    }
+  });
+});
