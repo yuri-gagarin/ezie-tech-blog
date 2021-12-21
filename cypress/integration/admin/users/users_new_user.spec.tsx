b0fb6257d6a96698e49cafd50895b333284f561c
Yuriy Ivanov <ymivanov@icloud.com>
2021-12-21 23:54:42 +0200

    added test for new user form open

--- cypress/integration/admin/users/users_new_user.spec.tsx
+++ cypress/integration/admin/users/users_new_user.spec.tsx
@@ -15,7 +15,7 @@ import type { ReqUserData } from "@/server/src/_types/users/userTypes"
 // helpers //
 import { deepCopyObject } from "@/components/_helpers/generalHelpers";
 import { checkEmptyObjVals } from "@/redux/_helpers/dataHelpers";
-import { capitalizeString, formatTimeString } from "../../../../components/_helpers/displayHelpers";
+import { capitalizeString, formatTimeString } from "@/components/_helpers/displayHelpers";
 import { generateMockUserData } from "@/server/spec/hepers/testHelpers";
 
 describe("Admin New Post page tests", () => {
@@ -28,7 +28,7 @@ describe("Admin New Post page tests", () => {
   let mockErrorResponse: ErrorUserRes;
   //
   let adminJWTToken: string;
-  let adminData: AdminData;
+  let adminUser: AdminData;
   //
   let adminIds: string[];
   let userIds: string[];
@@ -43,15 +43,15 @@ describe("Admin New Post page tests", () => {
         })
         .then(({ admins }) => {
           adminsArr = admins;
-          adminData = adminsArr[0];
+          adminUser = adminsArr[0];
           adminIds = admins.map((adminData) => adminData._id);
-          return cy.task<{ users: UserData[]}>("seedBlogPosts", { number: 10, role: userRole[0] })
+          return cy.task<{ users: UserData[]}>("seedBlogPosts", { number: 10, confirmed: true });
         })
         .then(({ users }) => { 
           usersArr = users;
           userIds = usersArr.map((userData) => userData._id);
-          const { email } = adminData;
-          return cy.request<LoginRes>("POST", "api/login",  { email, password: "password" })
+          const { email } = adminUser;
+          return cy.request<LoginRes>("POST", "api/login",  { email, password: "password" });
         })
         .then((loginData) => {
           const { jwtToken } = loginData.body;
@@ -66,4 +66,42 @@ describe("Admin New Post page tests", () => {
   before(() => {
     mockErrorResponse = { responseMsg: "Error Occured", error: new Error("Oooooops"), errorMessages: [ "An error occured" ]};
   });
+
+  beforeEach(() => {
+    cy.visit("/login")
+      .then(() => {
+        cy.getByDataAttr("login-page-email-input").type(adminUser.email);
+        cy.getByDataAttr("login-page-password-input").type("password");
+        cy.getByDataAttr("login-page-login-btn").click();
+        //
+        cy.getByDataAttr("admin-main-page").should("exist");
+        cy.wait(5000)
+        return cy.window().its("store").invoke("getState")
+      })
+      .then((state) => {
+        appState = deepCopyObject<IGeneralState>(state);
+        return cy.visit("/admin/dashboard/users/new");
+      })     
+      .then(() => {
+        cy.getByDataAttr("new-user-main-row").should("be.visible");
+      });        
+  });
+
+  it("Should render correct components and correct default values", () => {
+    cy.getByDataAttr("user-save-btn").should("be.visible").contains("Save");
+    cy.getByDataAttr("user-cancel-btn").should("be.visible").contains("Cancel");
+    //
+    cy.getByDataAttr("admin-user-form").should("exist").and("be.visible");
+    // assert correct blank form rendering //
+    cy.getByDataAttr("admin-user-form-first-name").should("be.visible").and("have.value", "");
+    cy.getByDataAttr("admin-user-form-last-name").should("be.visible").and("have.value", "");
+    cy.getByDataAttr("admin-user-form-email").should("be.visible").and("have.value", "");
+    cy.getByDataAttr("admin-user-form-password").should("be.visible").and("have.value", "");
+    cy.getByDataAttr("admin-user-form-password-confirm").should("be.visible").and("have-value", "");
+    //
+    // user state should not have a currently selected user //
+    // auth state should not change //
+    cy.window().its("store").invoke("getState").its("authState").should("deep.equal", appState.authState);
+    cy.window().its("store").invoke("getState").its("usersState").its("selectedUserData").should("deep.equal", appState.usersState.selectedUserData);
+  })
 });
