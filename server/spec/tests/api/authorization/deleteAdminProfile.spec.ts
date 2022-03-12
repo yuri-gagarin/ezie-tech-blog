1937a5ffa80a6fcd89c1e5f5214211d0acbb5a29
Yuriy Ivanov <ymivanov@icloud.com>
2022-03-12 23:51:57 +0200

    Add API test in <deleteAdminProfile.spec.ts> to test for API call w/o login

--- server/spec/tests/api/authorization/deleteAdminProfile.spec.ts
+++ server/spec/tests/api/authorization/deleteAdminProfile.spec.ts
@@ -32,10 +32,10 @@ describe("AuthController:deleteAdminProfile - Admin registration DELETE API test
   before(async () => {
     try {
       server = ServerInstance.getExpressServer();
-      await generateMockAdmins(1);
-      await generateMockUsers({ number: 2, confirmed: true });
-      adminUser = await Admin.findOne({});
-      ([ regUser, secondRegUser ] = await User.find({}));
+      const adminLevelAdmin = (await generateMockAdmins(2, "admin"))[0];
+      const onwerLevelAdmin = (await generateMockAdmins(2, "owner"))[0];
+      //
+      const regUser = (await generateMockUsers({ number: 1, confirmed: true })[0];
       // count models //
       numOfUserModels = await User.countDocuments();
     } catch (error) {
@@ -52,5 +52,36 @@ describe("AuthController:deleteAdminProfile - Admin registration DELETE API test
     ({ userJWTToken: regUserToken } = await loginUser({ chai, server, email: regUserEmail }));
     ({ userJWTToken: secondRegUserToken } = await loginUser({ chai, server, email: secondRegUserEmail }));
   });
+
+  // TEST DELETE /api/delete_admin_profile NO Login //
+  context("Admin Profile - DELETE - Admin NOT logged in", () => {
+    describe("DELETE /api/delete_admin_profile - Admin profile Delete VALID data NOT logged in", () => {
+      it("Should NOT delete Admin profile with WITHOUT a login and return a correct response", (done) => {
+        chai.request(server)
+          .delete("/api/delete_admin_profile")
+          .set({ Authorization: "" })
+          .send({ email: adminUserEmail, password: "password" })
+          .end((err, response) => {
+            if(err) done(err);
+            const { responseMsg, error, errorMessages } = response.body as RegisterRes;
+            expect(response.status).to.equal(401);
+            expect(responseMsg).to.be.a("string");
+            expect(error).to.be.an("object");
+            expect(errorMessages).to.be.an("array");
+            done();
+          });
+      });
+      it("Should NOT alter the number of <User> models in the database", async () => {
+        try {
+          const updatedNumOfUsers: number = await Admin.countDocuments();
+          expect(numOfUserModels).to.equal(updatedNumOfUsers);
+        } catch (error) {
+          throw error;
+        }
+      });
+    })
+  });
+  // END TEST DELETE /api/delete_addmin_profile NO Login //
+
 
 });
